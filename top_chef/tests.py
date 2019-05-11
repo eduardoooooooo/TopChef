9b0737794fe5f2da7ed0cee6a38ee4b2235f5679
eduardo <eduardoalexis.delgado01@estudiant.upf.edu>
2019-05-11 09:49:27 +0200

    Ficheros de prueba

--- top_chef/tests.py
+++ top_chef/tests.py
@@ -4,14 +4,14 @@ from TopChef.top_chef.controllers.word_dictionary import WordDictionary
 if __name__ == '__main__':
 
     topchef = TopChef()
-    topchef.load_data("../data/topchef_data.txt")
+    topchef.load_data("../data/topchef_data_alt.txt")
     word_dictionary = WordDictionary()
-    word_dictionary.load_words("../data/words.txt")
+    word_dictionary.load_words("../data/words_alt.txt")
     topchef.compute_reviews_score(word_dictionary)
     topchef.compute_recipes_score()
     topchef.compute_chefs_score()
     topchef.sort_structures()
-
+    """
     print("-------------SORTED REVIEWS--------------")
     a = topchef.reviews.sorted_reviews
     for i in range(len(a)):
@@ -29,3 +29,5 @@ if __name__ == '__main__':
 
     topchef.show_chefs(topchef.chefs.sorted_chefs)
     topchef.show_recipes(topchef.recipes.sorted_recipes)
+    """
+    topchef.show_chefs(topchef.chefs.sorted_chefs)
